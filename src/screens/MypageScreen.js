e171d103ed0655691d650b37d85ce33dbbceefc3
yoona1110 <gjdbsdk18@icloud.com>
2024-04-19 23:38:20 +0900

    Feat: mypage 대화기록 추가

--- src/screens/MypageScreen.js
+++ src/screens/MypageScreen.js
@@ -4,20 +4,35 @@ import styled from "styled-components";
 import palette from "../styles/colorPalette";
 
 import Logout from "../assets/icon/mypage/ic_logout.png";
+import ConverHistory from "../assets/icon/mypage/ConverHistory.png";
 import Logoutbutton from "../assets/icon/ic_backBtn.png";
 import MenuTitle from "../components/MenuTitle";
 
 const MyPage = () => {
     const navigation = useNavigation();
+    const category = [
+        {
+            title : "로그아웃",
+            movepage : "login",
+            type: "Logout"
+        },
+        {
+            title : "대화기록",
+            movepage : "login",
+            type: "ConverHistory"
+        }
+    ]
 
     return (
         <Container>
             <MenuTitle text={"MY페이지"} type={"main"}/>
-            <Body onPress={() => navigation.navigate("login")}>
-                <LogoutIcon source={Logout}/>
-                <LogoutText>로그아웃</LogoutText>
-                <LogoutBtn source={Logoutbutton}/>
-            </Body>
+            {category.map((item) => (
+                <SubContainer onPress={() => navigation.navigate("login")}>
+                    <Icon source={item.type} type={item.type}/>
+                    <LogoutText>{item.title}</LogoutText>
+                    <LogoutBtn source={Logoutbutton}/>
+                </SubContainer>
+            ))}
         </Container>
     );
 };
@@ -25,16 +40,26 @@ const Container = styled(SafeAreaView)`
     flex: 1;
     background-color: ${palette.white};
 `;
-const Body = styled.Pressable`
+const Body = styled.View`
+    display: flex;
+`;
+const SubContainer = styled.Pressable`
     display: flex;
     flex-direction: row;
     align-items: center;
     margin-top: 23px;
     padding-left: 28px;
 `;
-const LogoutIcon = styled.Image`
-    width: 24px;
-    height: 30px;
+const ConverHistoryContainer = styled.Pressable`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-top: 23px;
+    padding-left: 28px;
+`;
+const Icon = styled.Image`
+    width: ${(props) => (props.type === "Logout") ? "24px"  : "27px"};
+    height: ${(props) => (props.type === "Logout") ? "30px"  : "27px"};
 `;
 const LogoutText = styled.Text`
     font-size: 18px;
