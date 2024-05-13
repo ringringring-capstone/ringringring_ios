c8ece00f3130e52cd6a9af11d7b3a6ca7fd682a8
yoona1110 <gjdbsdk18@icloud.com>
2024-05-13 20:43:06 +0900

    Fix: initalRouteName 수정

--- App.js
+++ App.js
@@ -48,7 +48,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator 
-        initialRouteName="welcome"
+        initialRouteName="login"
         screenOptions={{
           headerShown: false,
       }}>
