0124e8f83f258d7b07216e2fdc3c1aa5005706d6
yoona1110 <gjdbsdk18@icloud.com>
2024-05-14 13:49:06 +0900

    Fix: 통화 종료 후, 재시도 시 seconds 오류 수정

--- src/components/callpractice/CallPractice.js
+++ src/components/callpractice/CallPractice.js
@@ -42,18 +42,22 @@ const CallPractice = ({route}) => {
             }, 1000);
         }
 
-        const reSubscribe = navigation.addListener('focus', () => {
+        const unsubscribe = navigation.addListener('blur', () => {
             clearInterval(intervalId);
-            if (!isClick) {
-                intervalId = setInterval(() => {
-                    setSeconds(prevSeconds => prevSeconds + 1);
-                }, 1000);
-            }
         });
+
+        // const reSubscribe = navigation.addListener('focus', () => {
+        //     clearInterval(intervalId);
+        //     if (!isClick) {
+        //         intervalId = setInterval(() => {
+        //             setSeconds(prevSeconds => prevSeconds + 1);
+        //         }, 1000);
+        //     }
+        // });
 
         return () => {
             clearInterval(intervalId);
-            reSubscribe();
+            unsubscribe();
         };
     }, [isClick]);
 
