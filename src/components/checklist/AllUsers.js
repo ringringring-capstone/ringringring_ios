33a851750e9021909d4480e8882b42faf164b50d
yoona1110 <gjdbsdk18@icloud.com>
2024-05-14 12:56:03 +0900

    Fix: 전체 이용자 통계 그래프 수정

--- src/components/checklist/AllUsers.js
+++ src/components/checklist/AllUsers.js
@@ -1,64 +1,62 @@
 import styled from "styled-components";
-import { PieChart } from "react-native-chart-kit";
+// import { PieChart } from "react-native-chart-kit";
+import { PieChart } from "react-native-gifted-charts";
 import palette from "../../styles/colorPalette";
+import ReuseText from "../ReuseText";
 
 
 const AllUsers = ({score}) => {
     const status = score >= 6 ? "심각" : (score >= 3 ? "위험" : "양호");
     const data = [
-        {
-          name: "심각",
-          population: 43,
-          color: status === "심각" ? `${palette.sub}` : "#D9D9D9",
-        },
-        {
-          name: "위험",
-          population: 62,
-          color: status === "위험" ? `${palette.sub}` : "#D9D9D9",
-        },
-        {
-          name: "양호",
-          population: 120,
-          color: status === "양호" ? `${palette.sub}` : "#D9D9D9",
-        },
-      ];
+      {
+        text: "심각",
+        value: 50,
+        shiftTextBackgroundX: 3
+      },
+      {
+        text: "위험",
+        value: 82,
+        shiftTextBackgroundX: 4,
+        shiftTextBackgroundY: -1,
+      },
+      {
+        text: "양호",
+        value: 30,
+        shiftTextBackgroundX: 3,
+        shiftTextBackgroundY: 0,
+      },
+    ];
     
     return (
         <Container>
-            <PieChart
-                data={data}
-                width={300}
-                height={220}
-                chartConfig={{
-                    backgroundGradientFrom: `${palette.white}`,
-                    backgroundGradientTo: `${palette.white}`,
-                    color: (opacity = 1) => `rgba(219, 219, 219, ${opacity})`,
-                    // strokeWidth: 2 // optional, default 3
-                }}
-                accessor={"population"}
-                backgroundColor={"transparent"}
-                paddingLeft={"15"}
-                hasLegend={false}
-                center={[50, -10]}
-                absolute
-            />
-            <EmptyCircle/>
+            <PieChart 
+              radius={100}
+              sectionAutoFocus={true}             // 선택된 데이터 확대
+              // showText                            // 차트 값 텍스트
+              // showValuesAsLabels={true}           // 값 보이게 할 지
+              labelsPosition={"onBorder"}         // 텍스트 위치
+              textColor={`${palette.black}`}      // 텍스트 색상
+              textSize={12}                       // 텍스트 크기
+              showTextBackground={true}
+              textBackgroundColor={`${palette.white}`}
+              textBackgroundRadius={30}
+              strokeWidth={2}                     // 그래프 테두리 두께
+              strokeColor={`${palette.white}`}    // 그래프 테두리 색상      
+              font={"IBMPlexSans-Regular"}
+              data={data.map((item) => ({
+                ...item,
+                percentage: item.value,
+                color: (item.text === status) ? `${palette.sub}` : "#D9D9D9",
+                focused: item.text === status,
+                // showValuesAsLabels: item.text === status,
+              }))}
+              donut/>
         </Container>
     );
 };
 const Container = styled.View`
     /* flex: 1; */
-    padding: 21px;
+    padding: 0 30px;
     margin: 7px 28px;
-`;
-const EmptyCircle = styled.View`
-    position: absolute;
-    display: flex;
-    top: 39%;
-    left: 43%;
-    width: 70px;
-    height: 70px;
-    background-color: ${palette.white};
-    border-radius: 50%;
 `;
 export default AllUsers;
